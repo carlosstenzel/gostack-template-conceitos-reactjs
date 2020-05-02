21e6dbf73c36c4585b67d7a62869cbedc3ba3a6e
Carlos Stenzel <carlosstenzel@hotmail.com>
2020-05-02 15:55:19 -0300

    Done :rocket:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = React.useState([]);
+
+  React.useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      "url": "https://github.com/carlosstenzel/Rocketseat",
+      "title": "Gostack da rocket com react",
+      "techs": ["node", "react"]
+    };
+
+    const response = await api.post("repositories", data);
+    const respo = response.data;
+    setRepositories([...repositories, respo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(respo => respo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((respo) => (
+          <li key={respo.id}>
+            {respo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(respo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
